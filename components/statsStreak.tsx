0cd0ea82d80a43c07a939616534584cda4fc0506
Yanis Rozier <yanis.rozier@orange.fr>
2026-01-04 18:30:20 +0100

    init statsStrak componenet without logic

--- components/statsStreak.tsx
+++ components/statsStreak.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function StatsStatut() {
     return (
@@ -6,20 +6,37 @@ export default function StatsStatut() {
             style={styles.container}
         >
 
+            <Image source={require('../assets/images/stats/streak/high.png')} style={styles.image} />
+            <Text style={styles.value}>12</Text>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
         backgroundColor: '#F1F1F1',
         width: '25%',
         height: '100%',
         borderRadius: 30,
         borderColor: 'rgba(0, 0, 15, 0.2)',
         borderWidth: 0.5,
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+    },
+
+    image: {
+        width: 60,
+        height: 60,
+        resizeMode: 'contain',
+    },
+
+    value: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#000',
     },
 });
